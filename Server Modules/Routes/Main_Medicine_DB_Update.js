c9a58faf0b559fad47e58f69db130bf68dff9d56
drkkgy <drkkgy@gmail.com>
2018-03-07 18:00:16 +0530

    Completed the user details module

--- Server Modules/Routes/Main_Medicine_DB_Update.js
+++ Server Modules/Routes/Main_Medicine_DB_Update.js
@@ -1,12 +1,49 @@
 //Dependencies
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient,assert = require('assert');
+
+//Connecting to the mongodb and setting it up
+
+
 
 // Routes
-router.get('/test', function(req,res,next){
+
+   //Server link testing page
+router.get('/test', (req,res,next)=>{
     
-    res.send('API working')
+    res.send('API working !!')
 });
+
+router.post('/uploading',(req,res,next)=>{
+
+// connecting and sending data to mongo db
+
+MongoClient.connect('mongodb://localhost:27017/Medicine_Database', (err,db)=> {
+
+    assert.equal(null,err);
+    console.log("Sucessfully connected to the mongodb client");
+    // sending the information
+  db.collection('Cardiovascular_System').update(
+  { Beta_Blockers: "Propranlol"},
+  { $set: { bose: 44 },
+     $currentDate: { lastModified: true }},
+     {upsert: true}
+     )
+.then(function(result) {
+  // process result
+  res.send(result);
+})            
+    
+});
+
+
+
+});
+
 
 //Return router
 
--- Server Modules/Routes/User_Personal_Details.js
+++ Server Modules/Routes/User_Personal_Details.js
@@ -1,12 +1,49 @@
 //Dependencies
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var router = express.Router();
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient,assert = require('assert');
+
 
 // Routes
 router.get('/test', function(req,res,next){
     
     res.send('API working')
 });
+
+router.post('/upload_user_details', (req,res,next)=>{
+
+
+	MongoClient.connect('mongodb://localhost:27017/User_Personal_Details', (err,db)=> {
+
+    assert.equal(null,err);
+    console.log("Sucessfully connected to the mongodb client");
+    // sending the information
+  db.collection('User_Details').insertOne({
+  "name": req.body.name,
+  "gender": req.body.gender,
+  "dob": req.body.dob,
+  "email": req.body.email,
+  "phoneNo": req.body.phoneNo,
+  "address": req.body.address,
+  "state": req.body.state,
+  "pinCode": req.body.pinCode
+})
+.then(function(result) {
+  // process result
+  res.json({"message": "Data uploaded sucessfully"});
+})
+    
+});
+
+
+
+});
+
+
+
 
 //Return router
 
